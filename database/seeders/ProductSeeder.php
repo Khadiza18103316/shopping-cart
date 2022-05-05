d645abe4bee7927bacd8f1041079b85b9b87d6c2
Khadiza <khadizaakhi756@gmail.com>
2022-05-05 23:38:13 +0600

    seeder update

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -14,28 +14,51 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
+            // Product::create(
+            // [
+            //     'name'=> 'Apple Mackbook Pro 16',
+            //     'details'=> 'Apple M1 Pro, 16 GPU, 16 GB, 512 Gb SSD',
+            //     'description'=> 'The most powerful MacBook Pro ever is here. With the blazing-fast M1 Pro or M1 Max chip — the first Apple silicon designed for pros — you get groundbreaking performance and amazing battery life. Add to that a stunning Liquid Retina XDR display, the best camera and audio ever in a Mac notebook, and all the ports you need. The first notebook of its kind, this MacBook Pro is a beast.',
+            //     'brand'=> 'Apple',
+            //     'price'=> 2499,
+            //     'shopping_cost'=> 25,
+            //     'image_path'=> 'storage/product.png'
+            // ]
+            // );
+            // Product::create(
+            // [
+            //     'name'=> 'Samsung Galaxy Book Pro',
+            //     'details'=> '13.3 inch, 8GB, DDR4, SDRAM, 256GB',
+            //     'description'=> 'PC power thats smartphone thin. Our lightest Galaxy Book yet gives you a powerful Intel® 11th Gen Core™ processor, Intel® Evo™ certification, an advanced AMOLED screen and comes equipped with our latest wi-fi chip. Finish important projects, download huge files fast or watch movies in brilliant color. Discover the perfect mix of portability and productivity.',
+            //     'brand'=> 'Samsung',
+            //     'price'=> 1400,
+            //     'shopping_cost'=> 25,
+            //     'image_path'=> 'storage/product2.png'
+            // ]
+            // );
+
        $products = [
            [
             'name'=> 'Apple Mackbook Pro 16',
             'details'=> 'Apple M1 Pro, 16 GPU, 16 GB, 512 Gb SSD',
-            'description'=> 'The most powerful MacBook Pro ever is here. With the blazing-fast M1 Pro or M1 Max chip — the first Apple silicon designed for pros — you get groundbreaking performance and amazing battery life. Add to that a stunning Liquid Retina XDR display, the best camera and audio ever in a Mac notebook, and all the ports you need. The first notebook of its kind, this MacBook Pro is a beast.',
+            'description'=> 'The most powerful MacBook Pro ever is here',
             'brand'=> 'Apple',
-            'price'=> 2499,
-            'shopping_cost'=> 25,
+            'price'=> '2499',
+            'shopping_cost'=> '25',
             'image_path'=> 'storage/product.png'
            ],
            [
             'name'=> 'Samsung Galaxy Book Pro',
             'details'=> '13.3 inch, 8GB, DDR4, SDRAM, 256GB',
-            'description'=> 'PC power thats smartphone thin. Our lightest Galaxy Book yet gives you a powerful Intel® 11th Gen Core™ processor, Intel® Evo™ certification, an advanced AMOLED screen and comes equipped with our latest wi-fi chip. Finish important projects, download huge files fast or watch movies in brilliant color. Discover the perfect mix of portability and productivity.',
+            'description'=> 'PC power thats smartphone thin',
             'brand'=> 'Samsung',
-            'price'=> 1400,
-            'shopping_cost'=> 25,
+            'price'=> '1400',
+            'shopping_cost'=> '25',
             'image_path'=> 'storage/product2.png'
         ],
     ];
     foreach($products as $key => $value){
         Product::create($value);
+     }
     }
     }
-}
